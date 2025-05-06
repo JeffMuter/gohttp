f7f7e08443b55154b08b94ed2e6c76b4a47ff952
JeffMuter <muterjeffery@gmail.com>
2025-05-06 19:57:50 +0000

    init db setup.

--- database/db.go
+++ database/db.go
@@ -1,5 +1,35 @@
 package database
 
+import (
+	"database/sql"
+	"log"
+)
+
+var db *sql.DB
+
+const schemaString string = `
+	CREATE TABLE IF NOT EXISTS submissions (
+	id INTEGER NOT NULL PRIMARY KEY,
+	was_valuable INTEGER NOT NULL,
+	recommend INTEGER NOT NULL,
+	sequel INTEGER NOT NULL,
+	pacing INTEGER NOT NULL,
+	comments TEXT
+  );`
+
 func InitDB() error {
+	var path string = "./gohttp.db"
+	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		log.Fatalf("database couldnt init: %v\n%v", err, db)
+	}
+
+	_, err = db.Exec(schemaString)
+	if err != nil {
+		log.Fatalf("database schema execution err: %v\n", err)
+	}
+
+	defer db.Close()
+
 	return nil
 }
--- main.go
+++ main.go
@@ -20,7 +20,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to initialize database: %v", err)
 	}
-	defer database.Close()
 
 	router := router.Router()
 
